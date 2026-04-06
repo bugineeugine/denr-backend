ead86e4ae42ce529ee4ed70924f2f4a5799da92c
Eugine Rosillon <eugine.rosillon@clicktekconsulting.com>
2026-04-06 23:17:42 +0800

    add env

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [ env('APP_URL_FRONTEND', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
